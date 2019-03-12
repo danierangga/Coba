b2f0c3fd7165440d23946963caadeea4306dc36f
danierangga <48473355+danierangga@users.noreply.github.com>
2019-03-12 17:35:14 +0700

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>Hello World</h1> <br/><table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0095</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>Daniel Rangga Bima</td>
+    </tr>
+</table>
+<a href="/unisbank/unisbank">Go To Akademik</a>
